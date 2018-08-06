56de2c9a1a32ec906e917c471fc19ca299d95d53
senel34 <joshuapohan93@gmail.com>
2018-08-06 20:25:33 +0700

    fixed missing db in import

--- app.py
+++ app.py
@@ -13,10 +13,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key  = 'joshua'
 api = Api(app)
 
-@app.before_first_request
-def create_tables():
-	db.create_all()
-
 jwt = JWT(app, authenticate, identity) #/auth , returns jwt token
 
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:5000/item/Name
